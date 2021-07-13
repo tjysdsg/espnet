6628ae2783df17413563fb6a989b4b81827c1efb
tjysdsg <tjy1018543509@163.com>
2021-07-13 18:10:12 +0800

    Add model checkpoint and early stopping to scoring mlp

--- egs2/ase/asr1/ase/scoring_mlp.py
+++ egs2/ase/asr1/ase/scoring_mlp.py
@@ -3,12 +3,12 @@ import os
 from speechocean762 import load_phone_symbol_table
 from ase_score import eval_scoring
 import pickle
-from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import numpy as np
 from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
 from ignite.metrics import Loss
+from ignite.handlers import Checkpoint, DiskSaver, EarlyStopping, global_step_from_engine
 from scoring_model import N_PHONES, load_data, data2array
 import torch
 import torch.nn as nn
@@ -22,8 +22,8 @@ def get_args():
     parser.add_argument('ref', metavar='REF', type=str, help='Reference file')
     parser.add_argument('--scores', type=str, help='Path to utt2scores')
     parser.add_argument('--phone-table', type=str, help='Path to phones-pure.txt')
-    parser.add_argument('--model-dir', type=str, default='tmp', help='Where to save the model')
-    parser.add_argument('--output-dir', type=str, default='tmp', help='Output directory')
+    parser.add_argument('--model-dir', type=str, default='tmp/scoring_model', help='Where to save the model')
+    parser.add_argument('--output-dir', type=str, default='tmp/scoring_model', help='Output directory')
     parser.add_argument('--use-probs', action='store_true', help='Whether HYP contains tokens or probability matrices')
     parser.add_argument('--include-1s', action='store_true', help='Exclude score-1 samples from training and testing')
     return parser.parse_args()
@@ -66,7 +66,7 @@ def calc_and_print_metrics(preds: np.ndarray, y: np.ndarray):
     return preds, mse, pcc
 
 
-def train_network(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader):
+def train_network(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader, output_dir: str):
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.8)
     criterion = nn.MSELoss()
     trainer = create_supervised_trainer(model, optimizer, criterion)
@@ -74,24 +74,42 @@ def train_network(model: nn.Module, train_loader: DataLoader, val_loader: DataLo
     val_metrics = {"loss": Loss(criterion)}
     evaluator = create_supervised_evaluator(model, metrics=val_metrics)
 
+    # save best
+    score_function = Checkpoint.get_default_score_fn("loss", score_sign=-1)
+    to_save = {'model': model}
+    handler = Checkpoint(
+        to_save,
+        DiskSaver(output_dir, create_dir=True, require_empty=False),
+        score_function=score_function,
+        score_name="loss",
+        filename_pattern='{name}.{ext}',
+        global_step_transform=global_step_from_engine(trainer),
+    )
+    evaluator.add_event_handler(Events.EPOCH_COMPLETED, handler)
+
+    # early stopping
+    handler = EarlyStopping(patience=5, score_function=score_function, trainer=trainer)
+    evaluator.add_event_handler(Events.COMPLETED, handler)
+
+    # log training acc per epoch
     @trainer.on(Events.EPOCH_COMPLETED)
     def log_training_results(trainer):
         evaluator.run(train_loader)
         metrics = evaluator.state.metrics
         print(f"Epoch[{trainer.state.epoch}]: train_loss: {metrics['loss']:.2f}")
 
+    # log eval acc per epoch
     @trainer.on(Events.EPOCH_COMPLETED)
     def log_validation_results(trainer):
         evaluator.run(val_loader)
         metrics = evaluator.state.metrics
         print(f"Epoch[{trainer.state.epoch}]: val_loss: {metrics['loss']:.2f}")
 
-    trainer.run(train_loader, max_epochs=50)
+    trainer.run(train_loader, max_epochs=200)
 
 
 def main():
     args = get_args()
-    model_path = os.path.join(args.model_dir, 'scoring.pkl')
     scaler_path = os.path.join(args.model_dir, 'scaler.pkl')
     os.makedirs(args.model_dir, exist_ok=True)
     os.makedirs(args.output_dir, exist_ok=True)
@@ -133,15 +151,16 @@ def main():
         val_loader = DataLoader(ProbMatrixDataset(x_val, y_val), batch_size=64, shuffle=True)
 
         mdl = ScoringModel()
-        train_network(mdl, train_loader, val_loader)
+        train_network(mdl, train_loader, val_loader, args.model_dir)
 
-        pickle.dump(mdl, open(model_path, 'wb'))
         pickle.dump(scaler, open(scaler_path, 'wb'))
     elif args.action == 'test':
         scaler = pickle.load(open(scaler_path, 'rb'))
         scaler.transform(X)
         test_loader = DataLoader(ProbMatrixDataset(X, Y), batch_size=64, shuffle=True)
-        mdl: ScoringModel = pickle.load(open(model_path, 'rb'))
+        model_path = os.path.join(args.model_dir, 'model.pt')
+        mdl = ScoringModel()
+        mdl.load_state_dict(torch.load(model_path))
 
         pred_all = []
         y_all = []
