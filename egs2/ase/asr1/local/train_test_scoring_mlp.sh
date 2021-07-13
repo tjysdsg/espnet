6c2e48b790c01a32d50057e127eab38230f8dd8f
tjysdsg <tjy1018543509@163.com>
2021-07-13 19:02:35 +0800

    Add automated mlp train test script

---
+++ egs2/ase/asr1/local/train_test_scoring_mlp.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+log() {
+  local fname=${BASH_SOURCE[1]##*/}
+  echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
+}
+
+stage=1
+stop_stage=10000
+scoring_opts=
+token_list=data/en_token_list/word/tokens.txt
+python=python3
+asr_exp=exp/asr_train_asr_transformer_raw_en_word_sp
+inference_tag=decode_asr_asr_model_valid.acc.best
+train_sets="so762_train libri_scoring_train"
+test_sets="so762_test"
+model_dir=exp/scoring_train # model save path
+aug_test_data=false
+aug_train_data=true
+
+log "$0 $*"
+
+. utils/parse_options.sh
+. ./path.sh
+. ./cmd.sh
+
+decode_dir="${asr_exp}/${inference_tag}"
+
+combine_data() {
+  sets=$1       # input data subsets
+  output_dir=$2 # output dir
+  hyp_file=$3   # token or probs
+
+  rm -rf ${output_dir}
+  mkdir -p ${output_dir}
+  for x in ${sets}; do
+    cat data/${x}/text >>${output_dir}/ref.txt
+    cat data/${x}/utt2scores >>${output_dir}/utt2scores
+    cat ${decode_dir}/${x}/${hyp_file} >>${output_dir}/hyp.txt
+  done
+}
+
+train_model() {
+  action=$1    # train/test
+  dir=$2       # data output dir
+  data_sets=$3 # datasets
+  aug=$4       # true/false
+  echo "train_model $1 $2 $3 $4"
+
+  hyp_file=probs
+
+  # combine train sets
+  combine_data "${data_sets}" ${dir} ${hyp_file}
+
+  # perform data aug
+  ref_file=${dir}/ref.txt
+  utt2scores=${dir}/utt2scores
+  if [ "${aug}" = "true" ]; then
+    ${python} ase/aug_scoring_data.py \
+      --text=${dir}/ref.txt \
+      --scores=${dir}/utt2scores \
+      --output-dir=${dir}
+
+    ref_file=${dir}/ref_aug.txt
+    utt2scores=${dir}/utt2scores_aug
+  fi
+
+  export PYTHONPATH=ase/
+  ${python} ase/scoring_model.py "${action}" \
+    ${dir}/hyp.txt \
+    ${ref_file} \
+    ${scoring_opts} \
+    --phone-table=${token_list} \
+    --scores=${utt2scores} \
+    --model-dir=${model_dir} \
+    --output-dir=${dir}
+}
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+  log "Stage 12: Training scoring model"
+  train_model train data/scoring_train "${train_sets}" ${aug_train_data}
+fi
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+  log "Stage 13: Testing scoring model"
+  train_model test data/scoring_test "${test_sets}" ${aug_test_data}
+fi
+
+# if [ ${stage} -ge 20 ]; then
+#   for dset in ${test_sets}; do
+#     _data="${data_feats}/${dset}"
+#     _dir="${asr_exp}/${inference_tag}/${dset}"
+#
+#     if [ "${dset}" = "test" ]; then # librispeech test
+#       ${python} ase/wer.py "${_dir}/token" "${_data}/text" --output-dir=${_dir}
+#     else # speechocean test
+#       # TODO: use data/so762/utt2scores and data/so762/text
+#       ${python} ase/ase_score.py "${_dir}/token" "local/speechocean762/text-phone" \
+#         --scores=local/speechocean762/scores.json --output-dir=${_dir}
+#       echo "Alignment results saved to ${_dir}/alignment.txt"
+#     fi
+#   done
+# fi
