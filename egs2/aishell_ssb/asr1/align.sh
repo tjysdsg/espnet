b7f564d1692ae2851a1a08fa4e71713ca7328dfd
tjysdsg <tjy1018543509@163.com>
2021-12-29 19:25:42 +0800

    Add test asr_align script

---
+++ egs2/aishell_ssb/asr1/align.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+log() {
+  local fname=${BASH_SOURCE[1]##*/}
+  echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
+}
+
+stage=1          # Processes starts from the specified stage.
+stop_stage=10000 # Processes is stopped at the specified stage.
+train_set="train"
+val_set="test"
+test_sets="test"
+nj=1
+
+asr_config=conf/tuning/train_asr_conformer_s3prlfrontend_wav2vec2.yaml
+exp_dir=exp/asr_train_asr_conformer_s3prlfrontend_wav2vec2_raw_word_sp
+out_dir=exp/asr_align_asr_conformer_s3prlfrontend_wav2vec2_raw_word_sp
+
+log "$0 $*"
+. utils/parse_options.sh
+
+if [ $# -ne 0 ]; then
+  log "Error: No positional arguments are required."
+  exit 2
+fi
+
+. ./path.sh
+. ./cmd.sh
+
+mkdir -p ${out_dir}
+
+${train_cmd} JOB=1:"${nj}" "${out_dir}"/asr_align.JOB.log \
+  python3 -m espnet2.bin.asr_align \
+  --asr_train_config ${asr_config} \
+  --asr_model_file ${exp_dir}/valid.acc.best.pth \
+  --fs 16000 \
+  --audio exp/SSB00050353.wav \
+  --text exp/text \
+  --output "${out_dir}/aligned.txt" || exit 1
