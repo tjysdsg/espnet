3251a7cf34e03b86290c68c80bd4cd570f293286
tjysdsg <tjy1018543509@163.com>
2022-02-28 21:06:36 +0800

    Reduce the number of jobs for regular machines

--- egs2/aishell_ssb/asr1/finetune_autism.sh
+++ egs2/aishell_ssb/asr1/finetune_autism.sh
@@ -15,7 +15,7 @@ inference_config=conf/decode_asr.yaml
 ./asr.sh \
   --ngpu 8 \
   --max_wav_duration 30 \
-  --inference_nj 300 \
+  --inference_nj 30 \
   --speed_perturb_factors "0.9 1.0 1.1" \
   --asr_config "${asr_config}" \
   --inference_config "${inference_config}" \
