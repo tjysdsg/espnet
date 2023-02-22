681761021bd2efb398a50b4e2daddae282729bef
ziang <ziangz@andrew.cmu.edu>
2023-02-22 17:40:20 -0500

    small fix

--- espnet2/tasks/gan_tts.py
+++ espnet2/tasks/gan_tts.py
@@ -523,17 +523,17 @@ class GANTTSTask(AbsTask):
                 tts=tts,
                 **args.model_conf,
             )
-
-        model = ESPnetGANTTSModel(
-            feats_extract=feats_extract,
-            normalize=normalize,
-            pitch_extract=pitch_extract,
-            pitch_normalize=pitch_normalize,
-            energy_extract=energy_extract,
-            energy_normalize=energy_normalize,
-            tts=tts,
-            **args.model_conf,
-        )
+        else:
+            model = ESPnetGANTTSModel(
+                feats_extract=feats_extract,
+                normalize=normalize,
+                pitch_extract=pitch_extract,
+                pitch_normalize=pitch_normalize,
+                energy_extract=energy_extract,
+                energy_normalize=energy_normalize,
+                tts=tts,
+                **args.model_conf,
+            )
         assert check_return_type(model)
         return model
 
