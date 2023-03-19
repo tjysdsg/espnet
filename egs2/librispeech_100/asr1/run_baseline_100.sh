4ba1977dc93c8ec3991d390bd8ee469099ad2a20
tjysdsg <tjy1018543509@163.com>
2023-03-19 16:28:53 -0400

    Add CCAT baseline scripts

--- egs2/librispeech_100/asr1/run_baseline_100.sh
+++ egs2/librispeech_100/asr1/run_baseline_100.sh
@@ -1,30 +1,27 @@
 #!/usr/bin/env bash
-# Set bash to 'debug' mode, it will exit on :
-# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
 set -e
 set -u
 set -o pipefail
+
+# Cyclic ASR-TTS ASR baseline trained on train_clean_100
 
 train_set="train_clean_100"
 valid_set="dev"
 test_sets="test_clean test_other dev_clean dev_other"
 
-asr_tag=conformer_lr2e-3_warmup15k_amp_nondeterministic
-asr_config=conf/train_asr.yaml
+asr_tag=ccat_baseline_100
+asr_config=conf/tuning/train_asr_conformer_lr2e-3_warmup15k_amp_nondeterministic.yaml
 inference_config=conf/decode_asr.yaml
 
 ./asr.sh \
-    --skip_data_prep false \
-    --skip_train false \
-    --skip_eval false \
     --lang en \
     --ngpu 1 \
     --nj 32 \
-    --inference_nj 32 \
-    --nbpe 5000 \
+    --inference_nj 100 \
+    --token_type char \
     --max_wav_duration 30 \
     --speed_perturb_factors "0.9 1.0 1.1" \
-    --audio_format "flac.ark" \
+    --audio_format "wav" \
     --feats_type raw \
     --use_lm false \
     --asr_tag "${asr_tag}" \
@@ -34,4 +31,4 @@ inference_config=conf/decode_asr.yaml
     --valid_set "${valid_set}" \
     --test_sets "${test_sets}" \
     --lm_train_text "data/${train_set}/text" \
-    --bpe_train_text "data/${train_set}/text" "$@"
+    "$@"
