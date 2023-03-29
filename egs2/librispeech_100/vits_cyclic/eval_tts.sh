1f90710c7ab61dad37bbffd8c17afe4a5c92434e
tjysdsg <tjy1018543509@163.com>
2023-03-29 00:55:05 -0400

    Add script for TTS evaluation

---
+++ egs2/librispeech_100/vits_cyclic/eval_tts.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+. ./path.sh
+
+decode_dir=SET_DECODE_DIR
+test_sets="dev_clean test_clean test_other"
+dumpdir=dump
+
+. utils/parse_options.sh
+
+for dset in ${test_sets}; do
+  # Evaluate MCD
+  ./pyscripts/utils/evaluate_mcd.py \
+    ${decode_dir}/${dset}/wav/wav.scp \
+    ${dumpdir}/raw/${dset}/wav.scp
+
+  # Evaluate log-F0 RMSE
+  ./pyscripts/utils/evaluate_f0.py \
+    ${decode_dir}/${dset}/wav/wav.scp \
+    ${dumpdir}/raw/${dset}/wav.scp
+done
