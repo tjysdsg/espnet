7e699c8de3203bf1cc238df5f4bd14fb8a11f264
tjysdsg <tjy1018543509@163.com>
2023-03-30 14:09:43 -0400

    Add VITS cyclic paired training

---
+++ egs2/librispeech_100/vits_cyclic/run_vits_paired.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+set -e
+set -u
+set -o pipefail
+
+fs=16000 # original 24000
+n_fft=1024
+n_shift=256
+win_length=null
+
+tag="vits_paired"
+
+train_set="train_clean_360"
+valid_set="dev_clean"
+test_sets="dev_clean test_clean test_other"
+
+train_config=conf/tuning/train_vits_paired_asr_weight_0.75.yaml
+inference_config=conf/decode.yaml
+inference_asr_config=conf/decode_asr.yaml
+inference_tts_config=conf/tuning/decode_vits.yaml
+
+
+./tts.sh \
+    --ngpu 1 \
+    --stage 6 \
+    --stop_stage 6 \
+    --inference_model valid.loss.best.pth \
+    --inference_nj 32 \
+    --use_multidecoder true \
+    --lang en \
+    --feats_type raw \
+    --fs "${fs}" \
+    --n_fft "${n_fft}" \
+    --n_shift "${n_shift}" \
+    --win_length "${win_length}" \
+    --use_xvector true \
+    --token_type char \
+    --cleaner none \
+    --tag "${tag}" \
+    --dumpdir dump \
+    --tts_task gan_tts \
+    --train_config "${train_config}" \
+    --g2p none \
+    --feats_extract linear_spectrogram \
+    --inference_config "${inference_config}" \
+    --inference_asr_config "${inference_asr_config}" \
+    --inference_tts_config "${inference_tts_config}" \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    --srctexts "data/${train_set}/text" \
+    --audio_format "wav" "$@"
