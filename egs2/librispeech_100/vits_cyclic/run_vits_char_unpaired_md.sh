9bdc0cf75de519d4e951fe309daa431d54857d9b
Ziang Zhou <ziangz@andrew.cmu.edu>
2023-04-22 12:02:26 -0400

    save unchanged configs to merge new changes

--- egs2/librispeech_100/vits_cyclic/run_vits_char_unpaired_md.sh
+++ egs2/librispeech_100/vits_cyclic/run_vits_char_unpaired_md.sh
@@ -11,13 +11,13 @@ n_fft=1024
 n_shift=256
 win_length=null
 
-tag="vits_unpaired_360_sudo"
+tag="vits_unpaired_360_sudo_asr_0_3_lr_1e_4"
 
 train_set="train_clean_360"
 valid_set="dev_clean"
-test_sets="dev_clean test_clean dev_other test_other"
+test_sets="dev_clean test_other test_clean"
 
-train_config=conf/tuning/train_vits_char_xvector_md_unpaired.yaml
+train_config=conf/tuning/train_vits_char_unpaired.yaml
 inference_config=conf/decode.yaml
 inference_asr_config=conf/decode_asr.yaml
 
@@ -26,7 +26,7 @@ inference_asr_config=conf/decode_asr.yaml
     --ngpu 1 \
     --stage 6 \
     --stop_stage 6 \
-    --inference_model valid.loss.ave.pth \
+    --inference_model latest.pth \
     --inference_nj 32 \
     --use_multidecoder true \
     --lang en \
