5651d76304f3f6567a14a0f0ec7fb8db1755b30f
Ziang Zhou <ziangz@andrew.cmu.edu>
2023-04-22 12:11:20 -0400

    current run script and config for paired 460h w/ asr_weight 0.75

---
+++ egs2/librispeech_100/vits_cyclic/run_vits_paired_freeze_disc_asr_0_75.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+set -e
+set -u
+set -o pipefail
+
+fs=16000 # original 24000
+n_fft=1024
+n_shift=256
+win_length=null
+
+tag="vits_paired_freeze_disc_asr_0_75_lr_1e_4"
+
+train_set="train_clean_460"
+valid_set="dev_clean"
+test_sets="dev_clean test_clean"
+
+train_config=conf/tuning/train_vits_char_paired_sa_freeze_disc_460h_asr0_75.yaml
+inference_tts_config=conf/decode.yaml
+inference_asr_config=conf/decode_asr.yaml
+
+
+./tts.sh \
+    --ngpu 1 \
+    --stage 6 \
+    --stop_stage 6 \
+    --inference_model latest.pth \
+    --inference_nj 32 \
+    --use_multidecoder true \
+    --lang en \
+    --feats_type raw \
+    --fs "${fs}" \
+    --n_fft "${n_fft}" \
+    --n_shift "${n_shift}" \
+    --win_length "${win_length}" \
+    --use_xvector true \
+    --token_type char \
+    --cleaner none \
+    --tag "${tag}" \
+    --dumpdir dump \
+    --expdir exp/norm_vits_cyclic \
+    --tts_task gan_tts \
+    --train_config "${train_config}" \
+    --g2p none \
+    --feats_extract linear_spectrogram \
+    --inference_tts_config "${inference_tts_config}" \
+    --inference_asr_config "${inference_asr_config}" \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    --srctexts "data/${train_set}/text" \
+    --audio_format "wav" "$@"
